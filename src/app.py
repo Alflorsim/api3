e5215ef798d8dcb1f746c852670fcc91d62c221b
Alfonso Lorbes <alfonsolorbes04@gmail.com>
2025-03-13 21:30:40 +0100

    comento lineas

--- src/app.py
+++ src/app.py
@@ -8,10 +8,10 @@ load_dotenv()
 app = Flask(__name__)
 
 
-app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
-app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
-app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
-app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+#app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+# app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+# app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+# app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['MONGO_URI'] = os.getenv('MONGO_URI')
 
